09c157f75c757a19c3e41bac28366b8bd2219219
shu_0115 <raisondetre0115@gmail.com>
2011-12-04 17:33:01 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -100,98 +100,18 @@ class ItemsController < ApplicationController
     @this_month_sum = @this_month_sum.sum( :price )
 
     @day_sum_array = Array.new
-    1.upto
+    1.upto(Time.now.end_of_month.day){ |day|
+      month_day = Time.local(Time.now.year, Time.now.month, day, 0, 0, 0)
+      day_sum = Item.where( :user_id => session[:user_id] )
+      day_sum = day_sum.where( "created_at >= '#{month_day.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at < '#{month_day.end_of_day.strftime("%Y-%m-%d %H:%M:%S")}'" )
+      day_sum = day_sum.sum( :price )
+      @day_sum_array[day] = day_sum
+    }
 
     @now = Time.now
     @wdays = ["日", "月", "火", "水", "木", "金", "土"]
     
     render :layout => false
   end
-
-
-
-=begin
-  # GET /items
-  # GET /items.xml
-  def index
-    @items = Item.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @items }
-    end
-  end
-
-  # GET /items/1
-  # GET /items/1.xml
-  def show
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @item }
-    end
-  end
-
-  # GET /items/new
-  # GET /items/new.xml
-  def new
-    @item = Item.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @item }
-    end
-  end
-
-  # GET /items/1/edit
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  # POST /items
-  # POST /items.xml
-  def create
-    @item = Item.new(params[:item])
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to(@item, :notice => 'Item was successfully created.') }
-        format.xml  { render :xml => @item, :status => :created, :location => @item }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /items/1
-  # PUT /items/1.xml
-  def update
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      if @item.update_attributes(params[:item])
-        format.html { redirect_to(@item, :notice => 'Item was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /items/1
-  # DELETE /items/1.xml
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(items_url) }
-      format.xml  { head :ok }
-    end
-  end
-=end
 
 end
