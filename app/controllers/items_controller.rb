e19e862d1cae73ac51ed949c245bf6bbadb1ee48
shu_0115 <raisondetre0115@gmail.com>
2011-12-04 15:52:50 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,11 +12,27 @@ class ItemsController < ApplicationController
     @today_sum = @items.sum( :price )
     @items = @items.order( "created_at DESC" ).all
 
-#    @today_sum = Item.where( :user_id => session[:user_id] )
-#    @today_sum = @today_sum.where( "created_at >= '#{Time.now.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at <= '#{Time.now.end_of_day.strftime("%Y-%m-%d %H:%M:%S")}'" )
-#    @today_sum = @today_sum.sum( :price )
-#    @today_sum = @items.sum( :price )
+    @this_month_sum = Item.where( :user_id => session[:user_id] )
+    @this_month_sum = @this_month_sum.where( "created_at >= '#{Time.now.beginning_of_month.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at < '#{Time.now.months_since(1).beginning_of_month.strftime("%Y-%m-%d %H:%M:%S")}'" )
+    @this_month_sum = @this_month_sum.sum( :price )
     
+    @now = Time.now
+    @wdays = ["日", "月", "火", "水", "木", "金", "土"]
+    
+    render :layout => false
+  end
+
+  #------#
+  # add2 #
+  #------#
+  def add2
+    @item = Item.new
+
+    @items = Item.where( :user_id => session[:user_id] )
+    @items = @items.where( "created_at >= '#{Time.now.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at <= '#{Time.now.end_of_day.strftime("%Y-%m-%d %H:%M:%S")}'" )
+    @today_sum = @items.sum( :price )
+    @items = @items.order( "created_at DESC" ).all
+
     @this_month_sum = Item.where( :user_id => session[:user_id] )
     @this_month_sum = @this_month_sum.where( "created_at >= '#{Time.now.beginning_of_month.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at < '#{Time.now.months_since(1).beginning_of_month.strftime("%Y-%m-%d %H:%M:%S")}'" )
     @this_month_sum = @this_month_sum.sum( :price )
@@ -31,6 +47,7 @@ class ItemsController < ApplicationController
   # create #
   #--------#
   def create
+    print "[ params ] : " ; p params ;
     @item = Item.new(params[:item])
 
     unless @item.blank?
@@ -45,6 +62,27 @@ class ItemsController < ApplicationController
 
     redirect_to :action => "add"
   end
+
+  #---------#
+  # create2 #
+  #---------#
+  def create2
+    @item = Item.new(params[:item])
+
+    unless @item.blank?
+      @item.user_id = session[:user_id]
+
+      unless @item.save
+        flash[:notice] = "項目の作成に失敗しました。"
+      end
+    else
+      flash[:notice] = "登録する情報がありません。"
+    end
+
+    redirect_to :action => "add"
+  end
+
+
 
 
 
