e988451f19eb7594dde31e9826a09bc12309a312
shu_0115 <raisondetre0115@gmail.com>
2011-12-07 18:12:31 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,14 @@ class ItemsController < ApplicationController
   #--------#
   def create
     print "[ params ] : " ; p params ;
-    @item = Item.new(params[:item])
+    
+    @item = Item.new( params[:item] )
+    
+    unless params[:date].blank?
+      @item.created_at = Time.parse( params[:date] )
+    else
+      @item.created_at = Time.now
+    end
 
     unless @item.blank?
       @item.user_id = session[:user_id]
@@ -46,7 +53,35 @@ class ItemsController < ApplicationController
       flash[:notice] = "登録する情報がありません。"
     end
 
-    redirect_to :action => "add"
+    from = params[:from]
+    from = "add" if from.blank?
+    redirect_to :action => from, :date => params[:date]
+  end
+
+  #-----#
+  # day #
+  #-----#
+  def day
+    @item = Item.new
+
+    unless params[:date].blank?
+      @this_day = Time.parse( params[:date] )
+    else
+      @this_day = Time.now
+    end
+print "[ @this_day ] : " ; p @this_day ;
+    @items = Item.where( :user_id => session[:user_id] )
+    @items = @items.where( "created_at >= '#{@this_day.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at <= '#{@this_day.end_of_day.strftime("%Y-%m-%d %H:%M:%S")}'" )
+    @today_sum = @items.sum( :price )
+    @items = @items.order( "created_at DESC" ).all
+
+    @this_month_sum = Item.where( :user_id => session[:user_id] )
+    @this_month_sum = @this_month_sum.where( "created_at >= '#{@this_day.beginning_of_month.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at < '#{@this_day.months_since(1).beginning_of_month.strftime("%Y-%m-%d %H:%M:%S")}'" )
+    @this_month_sum = @this_month_sum.sum( :price )
+
+    @now = Time.now
+    
+    render :layout => false
   end
 
   #-------#
@@ -76,12 +111,52 @@ class ItemsController < ApplicationController
     }
 
     @now = Time.now
-#    @wdays = ["日", "月", "火", "水", "木", "金", "土"]
     
     render :layout => false
   end
 
+  #------#
+  # year #
+  #------#
+  def year
+  end
 
+  #------#
+  # edit #
+  #------#
+  def edit
+    @item = Item.find( params[:id] )
+    
+    @items = Item.where( :user_id => session[:user_id] )
+    @items = @items.where( "created_at >= '#{@item.created_at.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S")}' AND created_at <= '#{@item.created_at.end_of_day.strftime("%Y-%m-%d %H:%M:%S")}'" )
+    @today_sum = @items.sum( :price )
+    @items = @items.order( "created_at DESC" ).all
+    
+    @this_day = @item.created_at
+  end
+
+  #--------#
+  # update #
+  #--------#
+  def update
+    @item = Item.where( :user_id => session[:user_id], :id => params[:id] ).first
+
+    unless @item.update_attributes( params[:item] )
+      flash[:notice] = "更新に失敗しました。"
+    end
+
+    redirect_to :action => "edit", :id => @item.id
+  end
+
+  #--------#
+  # delete #
+  #--------#
+  def delete
+    @item = Item.where( :user_id => session[:user_id], :id => params[:id] ).first
+    @item.destroy
+
+    redirect_to :action => "day", :date => @item.created_at
+  end
 
 
 
