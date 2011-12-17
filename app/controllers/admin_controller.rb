0c667ccb829f60d74c33effa051f9f06f1a46ace
shu0115 <raisondetre0115@gmail.com>
2011-12-18 00:09:50 +0900

    update

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 class AdminController < ApplicationController
+
   # 管理者以外リダイレクト
   before_filter :admin_authorize
 
@@ -7,7 +8,8 @@ class AdminController < ApplicationController
   # users #
   #-------#
   def users
-    @users = User.all
+    print "[ params ] : " ; p params ;
+    @users = User.all( :order => "id ASC" )
   end
 
   private
@@ -16,8 +18,16 @@ class AdminController < ApplicationController
   #-----------#
   def admin_authorize
     user = User.where( :id => session[:user_id] ).select( :twitter_id ).first
-print "[ user ] : " ; p user ;
-    redirect_to :root unless user.twitter_id.to_i == 14369656
+    
+    if defined? ADMIN_USER_TWITTER_ID_1
+      admin_twitter_id = ADMIN_USER_TWITTER_ID_1
+    elsif defined? ENV['ADMIN_USER_TWITTER_ID_1']
+      admin_twitter_id = ENV['ADMIN_USER_TWITTER_ID_1']
+    else
+      admin_twitter_id = 0
+    end
+    
+    redirect_to :root unless user.twitter_id.to_i == admin_twitter_id
   end
 
 end
